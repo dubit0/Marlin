337221bd10e9922cb8a8cb4a5b61c87c42815536
Thomas Niederprüm <niederp@physik.uni-kl.de>
2017-09-07 21:02:47 +0200

    Adapt z-axis steps/mm for TR8x2 lead screw
    Signed-off-by: Thomas Niederprüm <niederp@physik.uni-kl.de>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -552,7 +552,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 400, 2, 45 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 400, 3, 45 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
